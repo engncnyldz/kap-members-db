a0627699a83581f036620d8c7b8f5adc41b94910
engincan yıldız <engincanyldz@gmail.com>
2022-12-25 12:13:45 +0300

    get member router added

--- app/routers/member.py
+++ app/routers/member.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, status, HTTPException
 from .. import models
 from ..database import get_db
 from sqlalchemy.orm import Session
@@ -9,13 +9,22 @@ router = APIRouter(
     prefix="/member"
 )
 
-@router.get("/", response_model=List[models.MemberModel])
+@router.get("/", status_code=status.HTTP_200_OK, response_model=List[models.MemberModel])
 def get_all_members(db: Session = Depends(get_db)):
 
     members = db.query(models.MemberOrm).all()
     return members
 
-@router.post("/", response_model=models.InsertUpdateResult)
+@router.get("/{code}", status_code=status.HTTP_200_OK, response_model=models.MemberModel)
+def get_member(code: str, db: Session = Depends(get_db)):
+
+    member = db.query(models.MemberOrm).filter(models.MemberOrm.stock_code.contains(code.upper())).first()
+    if member:
+        return member
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{code} not found.")
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=models.InsertUpdateResult)
 def sync_members():
     members = query_members()
     result = insert_or_update_members(members)
